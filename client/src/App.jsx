a5d914dea2a2ad2270ff910fd78aee3a40b4674e
Konete326 <konete326@github.com>
2026-06-20 19:45:02 +0500

    fix: use HashRouter in Electron to fix 404 and bump version to 1.0.2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -80,16 +80,18 @@ function Router() {
 }
 
 export default function App() {
+  const RouterComponent = window.location.protocol === 'file:' ? HashRouter : BrowserRouter;
+
   return (
     <AuthProvider>
-      <BrowserRouter>
+      <RouterComponent>
         <QueryClientProvider client={queryClient}>
           <TooltipProvider>
             <Toaster />
             <Router />
           </TooltipProvider>
         </QueryClientProvider>
-      </BrowserRouter>
+      </RouterComponent>
     </AuthProvider>
   );
 }
